947fade873f4746a2f2fb419f235cf9ce3bbae0d
Bruno Silva <bruno.silva.t0126205@edu.atec.pt>
2024-03-14 14:09:16 +0000

    final

---
+++ rockpaperscissors.js
@@ -0,0 +1,53 @@
+const playerScore = document.querySelector(".player-point");
+const computerScore = document.querySelector(".computer-point");
+const command = document.querySelector(".command");
+const buttons = document.querySelectorAll("button")
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+
+let playerChoice = ""
+
+
+buttons.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        playerChoice = item.className
+        playRound(playerChoice, getComputerChoice())
+        if (playerScore.textContent == 5) {
+            h1.textContent = "You Won!"
+            command.remove()
+        } else if (computerScore.textContent == 5) {
+            h1.textContent = "You Lost!"
+            command.remove()
+        }
+    })
+})
+
+function getComputerChoice() {
+    const compChoice = Math.floor(Math.random() * 3) + 1
+    switch (compChoice) {
+        case 1:
+            return "paper"
+        case 2:
+            return "scissor"
+        default:
+            return "rock"
+    }
+}
+
+function playRound(playerChoice, compChoice) {
+    if (playerChoice == compChoice) {
+        alert("Tie!")
+    } else if (playerChoice == "rock" && compChoice == "paper") {
+        alert("You Lost! Paper beats Rock")
+        computerScore.textContent++
+    } else if (playerChoice == "paper" && compChoice == "scissor") {
+        alert("You Lost! Scissor beats Paper")
+        computerScore.textContent++
+    } else if (playerChoice == "scissor" && compChoice == "rock") {
+        alert("You Lost! Rock beats Scissor")
+        computerScore.textContent++
+    } else {
+        alert(`You Won! ${playerChoice} beats ${compChoice}`)
+        playerScore.textContent++
+    }
+}
